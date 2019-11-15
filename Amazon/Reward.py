4b020ddc5699eb9a9678c176af15433747107134
Ryan Schreiber <Ryan.Schreiber@datacom.co.nz>
2019-11-15 17:18:26 +1300

    Modify project to use Azure

---
+++ Amazon/Reward.py
@@ -0,0 +1,11 @@
+﻿def reward_function(params):
+    import json
+    import urllib.request
+    url = 'https://app-sw-dr.azurewebsites.net/reward'
+    data_string = json.dumps (params)
+    data = bytes(data_string,'utf-8')
+    headers = {'Content-Type': 'application/json'}
+    request = urllib.request.Request (url=url,data=data, headers=headers, method='POST')
+    response = urllib.request.urlopen (request)
+    reward = float((str(response.read(),'utf-8')))
+    return float(reward)
---
+++ Models/RewardRequestModel.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+
+namespace DeepRacerProcessor.Models
+{
+    public partial class RewardRequestModel
+    {
+        [JsonProperty("all_wheels_on_track")]
+        public bool AllWheelsOnTrack { get; set; }
+
+        [JsonProperty("x")]
+        public double X { get; set; }
+
+        [JsonProperty("y")]
+        public double Y { get; set; }
+
+        [JsonProperty("distance_from_center")]
+        public long DistanceFromCenter { get; set; }
+
+        [JsonProperty("heading")]
+        public double Heading { get; set; }
+
+        [JsonProperty("progress")]
+        public long Progress { get; set; }
+
+        [JsonProperty("steps")]
+        public long Steps { get; set; }
+
+        [JsonProperty("speed")]
+        public long Speed { get; set; }
+
+        [JsonProperty("steering_angle")]
+        public long SteeringAngle { get; set; }
+
+        [JsonProperty("track_width")]
+        public double TrackWidth { get; set; }
+
+        [JsonProperty("waypoints")]
+        public double[][] Waypoints { get; set; }
+
+        [JsonProperty("closest_waypoints")]
+        public long[] ClosestWaypoints { get; set; }
+
+        [JsonProperty("is_left_of_center")]
+        public bool IsLeftOfCenter { get; set; }
+
+        [JsonProperty("is_reversed")]
+        public bool IsReversed { get; set; }
+    }
+
+    public partial class RewardRequestModel
+    {
+        public static RewardRequestModel FromJson(string json) => JsonConvert.DeserializeObject<RewardRequestModel>(json);
+    }
+}
